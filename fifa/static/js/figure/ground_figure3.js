a21e0433908279e500c5ddbdd8331d1f9530b176
ZhihaoXu <117010330@link.cuhk.edu.cn>
2019-12-05 00:18:54 +0800

    add dashboard index

---
+++ fifa/static/js/figure/ground_figure3.js
@@ -0,0 +1,437 @@
+var ground_figure3 = {
+    "data": [
+        {
+            "uid": "05f04e29-1825-49dd-8f9c-d0ada4818b21",
+            "meta": {
+                "columnNames": {
+                    "x": "GK, x",
+                    "y": "GK, y; CB, y; CAM, y",
+                    "text": "GK, text"
+                }
+            },
+            "mode": "markers",
+            "name": "GK",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:a499e1",
+            "x": [
+                12
+            ],
+            "ysrc": "abuzhidaoa8:14:49bcae",
+            "y": [
+                25
+            ],
+            "marker": {
+                "size": 12,
+                "color": "pink"
+            },
+            "textsrc": "abuzhidaoa8:14:2cd47b",
+            "text": [
+                "De Gea"
+            ]
+        },
+        {
+            "uid": "154cdb0d-110e-4e58-8e99-2ff4ea21b21c",
+            "meta": {
+                "columnNames": {
+                    "x": "LCB, x; RCB, x; CB, x",
+                    "y": "LCB, y; RS, y; LDM, y",
+                    "text": "LCB, text"
+                }
+            },
+            "mode": "markers",
+            "name": "LCB",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:ec5d76",
+            "x": [
+                30
+            ],
+            "ysrc": "abuzhidaoa8:14:af8247",
+            "y": [
+                20
+            ],
+            "marker": {
+                "size": 12,
+                "color": "red"
+            },
+            "textsrc": "abuzhidaoa8:14:f57aa5",
+            "text": [
+                "G. Chiellini"
+            ]
+        },
+        {
+            "uid": "53838591-f4c1-4008-b21f-f6704883d8b4",
+            "meta": {
+                "columnNames": {
+                    "x": "LCB, x; RCB, x; CB, x",
+                    "y": "RCB, y",
+                    "text": "RCB, text"
+                }
+            },
+            "mode": "markers",
+            "name": "RCB",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:ec5d76",
+            "x": [
+                30
+            ],
+            "ysrc": "abuzhidaoa8:14:46173d",
+            "y": [
+                40
+            ],
+            "marker": {
+                "size": 12,
+                "color": "red"
+            },
+            "textsrc": "abuzhidaoa8:14:8e198e",
+            "text": [
+                "Sergio Ramos"
+            ]
+        },
+        {
+            "uid": "98abc5cb-5a75-4d41-936d-50c872ef538f",
+            "meta": {
+                "columnNames": {
+                    "x": "LCB, x; RCB, x; CB, x",
+                    "y": "GK, y; CB, y; CAM, y",
+                    "text": "CB, text"
+                }
+            },
+            "mode": "markers",
+            "name": "CB",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:ec5d76",
+            "x": [
+                30
+            ],
+            "ysrc": "abuzhidaoa8:14:49bcae",
+            "y": [
+                25
+            ],
+            "marker": {
+                "size": 12,
+                "color": "red"
+            },
+            "textsrc": "abuzhidaoa8:14:a7936a",
+            "text": [
+                "D. Godín"
+            ]
+        },
+        {
+            "uid": "2e2d91dd-6310-4430-bed1-f4e6dfa43f92",
+            "meta": {
+                "columnNames": {
+                    "x": "LAM, x; RAM, x",
+                    "y": "LAM, y",
+                    "text": "LAM, text"
+                }
+            },
+            "mode": "markers",
+            "name": "LAM",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:fcd63c",
+            "x": [
+                50
+            ],
+            "ysrc": "abuzhidaoa8:14:9430b5",
+            "y": [
+                50
+            ],
+            "marker": {
+                "size": 12,
+                "color": "blue"
+            },
+            "textsrc": "abuzhidaoa8:14:507a30",
+            "text": [
+                "J. Rodríguez"
+            ]
+        },
+        {
+            "uid": "44d2ca6f-b780-43ae-bba3-a51c30d159de",
+            "meta": {
+                "columnNames": {
+                    "x": "RDM, x; LDM, x",
+                    "y": "RDM, y; LS, y",
+                    "text": "RDM, text"
+                }
+            },
+            "mode": "markers",
+            "name": "RDM",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:460732",
+            "x": [
+                60
+            ],
+            "ysrc": "abuzhidaoa8:14:43a529",
+            "y": [
+                30
+            ],
+            "marker": {
+                "size": 12,
+                "color": "yellow"
+            },
+            "textsrc": "abuzhidaoa8:14:07de34",
+            "text": [
+                "P. Pogba"
+            ]
+        },
+        {
+            "uid": "37e74a5a-814d-4756-b21b-5cdff7011a33",
+            "meta": {
+                "columnNames": {
+                    "x": "LAM, x; RAM, x",
+                    "y": "RAM, y",
+                    "text": "RAM, text"
+                }
+            },
+            "mode": "markers",
+            "name": "RAM",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:fcd63c",
+            "x": [
+                50
+            ],
+            "ysrc": "abuzhidaoa8:14:838ff4",
+            "y": [
+                2
+            ],
+            "marker": {
+                "size": 12,
+                "color": "blue"
+            },
+            "textsrc": "abuzhidaoa8:14:66d8fd",
+            "text": [
+                "J. Cuadrado"
+            ]
+        },
+        {
+            "uid": "1794d96d-5df5-497c-b8c7-2cae04eda20d",
+            "meta": {
+                "columnNames": {
+                    "x": "CAM, x",
+                    "y": "GK, y; CB, y; CAM, y",
+                    "text": "CAM, text"
+                }
+            },
+            "mode": "markers",
+            "name": "CAM",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:0e5fd0",
+            "x": [
+                70
+            ],
+            "ysrc": "abuzhidaoa8:14:49bcae",
+            "y": [
+                25
+            ],
+            "marker": {
+                "size": 12,
+                "color": "yellow"
+            },
+            "textsrc": "abuzhidaoa8:14:403bdb",
+            "text": [
+                "A. Griezmann"
+            ]
+        },
+        {
+            "uid": "9a31e70e-94fb-4697-859b-5628eb0de9d6",
+            "meta": {
+                "columnNames": {
+                    "x": "RS, x; LS, x",
+                    "y": "LCB, y; RS, y; LDM, y",
+                    "text": "RS, text"
+                }
+            },
+            "mode": "markers",
+            "name": "RS",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:3ac512",
+            "x": [
+                90
+            ],
+            "ysrc": "abuzhidaoa8:14:af8247",
+            "y": [
+                20
+            ],
+            "marker": {
+                "size": 12,
+                "color": "green"
+            },
+            "textsrc": "abuzhidaoa8:14:b09d73",
+            "text": [
+                "L. Suárez"
+            ]
+        },
+        {
+            "uid": "530107a9-ef86-4b15-979f-2e4acb583650",
+            "meta": {
+                "columnNames": {
+                    "x": "RS, x; LS, x",
+                    "y": "RDM, y; LS, y",
+                    "text": "LS, text"
+                }
+            },
+            "mode": "markers",
+            "name": "LS",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:3ac512",
+            "x": [
+                90
+            ],
+            "ysrc": "abuzhidaoa8:14:43a529",
+            "y": [
+                30
+            ],
+            "marker": {
+                "size": 12,
+                "color": "green"
+            },
+            "textsrc": "abuzhidaoa8:14:a68c34",
+            "text": [
+                "E. Cavani"
+            ]
+        },
+        {
+            "uid": "afd08be7-6445-41bf-b2f6-2bf78c204712",
+            "meta": {
+                "columnNames": {
+                    "x": "RDM, x; LDM, x",
+                    "y": "LCB, y; RS, y; LDM, y",
+                    "text": "LDM, text"
+                }
+            },
+            "mode": "markers",
+            "name": "LDM",
+            "type": "scatter",
+            "xsrc": "abuzhidaoa8:14:460732",
+            "x": [
+                60
+            ],
+            "ysrc": "abuzhidaoa8:14:af8247",
+            "y": [
+                20
+            ],
+            "marker": {
+                "size": 12,
+                "color": "yellow"
+            },
+            "textsrc": "abuzhidaoa8:14:d93188",
+            "text": [
+                "N. Kanté"
+            ]
+        }
+    ],
+    "layout": {
+        "title": {
+            "text": ""
+        },
+        "xaxis": {
+            "type": "linear",
+            "range": [
+                0,
+                120
+            ],
+            "ticks": "",
+            "mirror": true,
+            "showgrid": false,
+            "showline": true,
+            "tickmode": "array",
+            "ticktext": [
+                "Goal",
+                "Goal"
+            ],
+            "tickvals": [
+                10,
+                110
+            ],
+            "zeroline": false,
+            "autorange": false,
+            "linecolor": "black",
+            "linewidth": 1,
+            "showticklabels": true
+        },
+        "yaxis": {
+            "type": "linear",
+            "range": [
+                -3.3,
+                56.3
+            ],
+            "ticks": "",
+            "title": {
+                "text": ""
+            },
+            "mirror": true,
+            "showgrid": false,
+            "showline": true,
+            "zeroline": false,
+            "autorange": false,
+            "linecolor": "black",
+            "linewidth": 1,
+            "showticklabels": false
+        },
+        "shapes": [
+            {
+                "x0": 0,
+                "x1": 120,
+                "y0": 0,
+                "y1": 0,
+                "line": {
+                    "color": "white",
+                    "width": 2
+                },
+                "type": "line",
+                "layer": "below"
+            },
+            {
+                "x0": 0,
+                "x1": 120,
+                "y0": 53.3,
+                "y1": 53.3,
+                "line": {
+                    "color": "white",
+                    "width": 2
+                },
+                "type": "line",
+                "layer": "below"
+            },
+            {
+                "x0": 50,
+                "x1": 50,
+                "y0": 0,
+                "y1": 53.3,
+                "line": {
+                    "color": "white"
+                },
+                "type": "line",
+                "layer": "below"
+            },
+            {
+                "x0": 10,
+                "x1": 10,
+                "y0": 0,
+                "y1": 53.3,
+                "line": {
+                    "color": "white",
+                    "width": 10
+                },
+                "type": "line",
+                "layer": "below"
+            },
+            {
+                "x0": 110,
+                "x1": 110,
+                "y0": 0,
+                "y1": 53.3,
+                "line": {
+                    "color": "white",
+                    "width": 10
+                },
+                "type": "line",
+                "layer": "below"
+            }
+        ],
+        "autosize": true,
+        "showlegend": true,
+        "plot_bgcolor": "darkseagreen"
+    },
+    "frames": []
+}
