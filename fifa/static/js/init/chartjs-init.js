de027a97df765616b673c5f5819213704f8cfd66
Erdi Gao <39030290+a984517125@users.noreply.github.com>
2019-12-08 13:54:20 +0800

    123

--- fifa/static/js/init/chartjs-init.js
+++ fifa/static/js/init/chartjs-init.js
@@ -68,74 +68,74 @@ function radar_charts_update_data(data) {
     });
 }
 
-function radar_charts2_update_data(data) {
-    var ctx = document.getElementById( "radarChart2" );
-    ctx.height = 160;
-    var myChart = new Chart( ctx, {
-        type: 'radar',
-        data: {
-            labels: [ "PAC",  "SHO", 
-            "PAS", "DRI", "DEF", "PHY" ],
-            datasets: [
-                {
-                    label: "Player 1",
-                    data: data[0],
-                    fill:true,
-                    pointBackgroundColor:"rgba(0, 139, 69, 0.7)",
-                    borderColor: "rgba(0, 194, 146, 0.7)",
-                    borderWidth: "1",
-                    backgroundColor: "rgba(0, 194, 146, 0.4)"
-                            },
-                 {
-                     label: "Player 2",
-                     data: data[1],
-                     borderColor: "rgba(255, 0, 0, 0.7)",
-                     borderWidth: "1",
-                     backgroundColor: "rgba(255, 0, 0, 0.4)"
-                             }
-                         ]
-        },
-        options: {
-            legend: {
-                position:"bottom",
-            },
-            scale: {
-                angleLines: {
-                    display: true,
-                },
-                ticks: {
-                    beginAtZero: true
-                }
-            },
-            tooltips: {
-                mode: 'label',
-                enabled: true,
-                callbacks: {
-                    label: function(tooltipItem, data) {
-                        // var label = data.datasets[tooltipItem.datasetIndex].label || '';
-                        // var label = tooltipItem.datasetIndex;
-                        var label;
-                        var labels =  [
-                        "Pace",
-                        "Shooting",
-                        "Passing",
-                        "Dribbling",
-                        "Defending",
-                        "Physical"
-                        ];
+// function radar_charts2_update_data(data) {
+//     var ctx = document.getElementById( "radarChart2" );
+//     ctx.height = 160;
+//     var myChart = new Chart( ctx, {
+//         type: 'radar',
+//         data: {
+//             labels: [ "PAC",  "SHO", 
+//             "PAS", "DRI", "DEF", "PHY" ],
+//             datasets: [
+//                 {
+//                     label: "Player 1",
+//                     data: data[0],
+//                     fill:true,
+//                     pointBackgroundColor:"rgba(0, 139, 69, 0.7)",
+//                     borderColor: "rgba(0, 194, 146, 0.7)",
+//                     borderWidth: "1",
+//                     backgroundColor: "rgba(0, 194, 146, 0.4)"
+//                             },
+//                  {
+//                      label: "Player 2",
+//                      data: data[1],
+//                      borderColor: "rgba(255, 0, 0, 0.7)",
+//                      borderWidth: "1",
+//                      backgroundColor: "rgba(255, 0, 0, 0.4)"
+//                              }
+//                          ]
+//         },
+//         options: {
+//             legend: {
+//                 position:"bottom",
+//             },
+//             scale: {
+//                 angleLines: {
+//                     display: true,
+//                 },
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             },
+//             tooltips: {
+//                 mode: 'label',
+//                 enabled: true,
+//                 callbacks: {
+//                     label: function(tooltipItem, data) {
+//                         // var label = data.datasets[tooltipItem.datasetIndex].label || '';
+//                         // var label = tooltipItem.datasetIndex;
+//                         var label;
+//                         var labels =  [
+//                         "Pace",
+//                         "Shooting",
+//                         "Passing",
+//                         "Dribbling",
+//                         "Defending",
+//                         "Physical"
+//                         ];
                         
-                        label = labels[tooltipItem.index] + ": ";
-                        label += Math.round(tooltipItem.yLabel * 100) / 100;
+//                         label = labels[tooltipItem.index] + ": ";
+//                         label += Math.round(tooltipItem.yLabel * 100) / 100;
                         
-                        // return [label , labels[tooltipItem.index][0], labels[tooltipItem.index][1]];
-                        return label;
-                    }
-                }    
-            }    
+//                         // return [label , labels[tooltipItem.index][0], labels[tooltipItem.index][1]];
+//                         return label;
+//                     }
+//                 }    
+//             }    
 
-        }
-    });
-}
+//         }
+//     });
+// }
 
 
 
@@ -580,33 +580,33 @@ function fetch_gk_and_change(id){
     )
 }
 
-function fetch_similar_and_change(id1,id2) {
-    // console.log(234)
-    fetch("/" + id1 + "/" + id2 + "/similar_player_score").then(
-        function (response) {
-            return response.json()
-        }
-    ).then(
-        function (data) {
-            radar_charts2_update_data(data);
-            // console.log(data);
-        }
-    )
-}
+// function fetch_similar_and_change(id1,id2) {
+//     // console.log(234)
+//     fetch("/" + id1 + "/" + id2 + "/similar_player_score").then(
+//         function (response) {
+//             return response.json()
+//         }
+//     ).then(
+//         function (data) {
+//             radar_charts2_update_data(data);
+//             // console.log(data);
+//         }
+//     )
+// }
 
 
 
 (function ($) {
     "use strict";
     var id_value = parseInt(document.getElementById('empty').name)
-    var id_value1 = parseInt(document.getElementById('empty1').name)
+    // var id_value1= parseInt(document.getElementById('empty1').name)
 
     console.log(id_value)
-    console.log(id_value1)
+
 
     //radar chart
     fetch_data_and_change(id_value)
-    fetch_similar_and_change(id_value,id_value1)
+    // fetch_similar_and_change(id_value,id_value1)
     //polar chart
     fetch_passing_and_change(id_value)
     fetch_dribbling2_and_change(id_value)
