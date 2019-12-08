04f126ce83a068be397e7e7f7015fafe1047a7fd
ZhihaoXu <117010330@link.cuhk.edu.cn>
2019-12-08 19:56:34 +0800

    add tooltips

--- .history/fifa/static/js/init/similar_charts_20191208195157.js
+++ .history/fifa/static/js/init/similar_charts_20191208195157.js
@@ -55,8 +55,9 @@ function radar_charts2_update_data(data,chart_id) {
                         "Physical"
                         ];
                         
-                        label = data.datasets[tooltipItem.datasetIndex].label + ": ";
+                        label = labels[tooltipItem.index] + ": ";
                         label += Math.round(tooltipItem.yLabel * 100) / 100;
+                        label += data.datasets.data[tooltipItem.datasetIndex][0];
                         
                         // return [label , labels[tooltipItem.index][0], labels[tooltipItem.index][1]];
                         return label;
